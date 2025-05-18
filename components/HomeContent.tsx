ef552ec089916cdce3feee169a3a9a53520626c2
Aarchi Kumari <aarchikumari07052@gmail.com>
2025-05-19 00:21:04 +0530

    makes Navbar and MobileMenu components

--- components/HomeContent.tsx
+++ components/HomeContent.tsx
@@ -1,19 +1,12 @@
 'use client';
 
 import React, { use } from 'react'
-import useCurrentUser from "@/hooks/useCurrentUser";
-import { signOut } from "next-auth/react";
+import Navbar from './Navbar'
 
 const HomeContent = () => {
-    
-  const {data: user} = useCurrentUser();
   return (
-     <>
-      <h1 className="text-4xl text-green-500">Netflix Clone</h1>
-      <p className="text-red-900">Logged in as: {user?.name}</p>
-      <button className="h-10 w-full bg-white" onClick={() => signOut()}>
-      Logout
-      </button>
+    <>
+      <Navbar />
     </>
   )
 }
