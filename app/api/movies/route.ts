18699905d27a8a052ed8c1f84df2cfbedb02ce91
Aarchi Kumari <aarchikumari07052@gmail.com>
2025-05-21 22:47:43 +0530

    MovieList

---
+++ app/api/movies/route.ts
@@ -0,0 +1,16 @@
+import { NextResponse } from 'next/server';
+import prismadb from "@/lib/prismadb";
+import serverAuth from "@/lib/serverAuth";
+
+export async function GET() {
+    try {
+        await serverAuth();
+
+        const movies = await prismadb.movie.findMany(); 
+        
+        return NextResponse.json(movies);
+    } catch (error) {
+        console.log(error);
+        return new NextResponse('Internal Error', { status: 400 });
+    }
+}
--- components/HomeContent.tsx
+++ components/HomeContent.tsx
@@ -3,12 +3,19 @@
 import React, { use } from 'react'
 import Navbar from './Navbar'
 import Billboard from './Billboard';
+import MovieList from './MovieList';
+import useMovieList from '@/hooks/useMovieList';
 
 const HomeContent = () => {
+  const { data: movies = [] } = useMovieList();
+
   return (
     <>
       <Navbar />
       <Billboard />
+      <div className='pb-40'>
+        <MovieList title="Trending Now" data={movies} />
+      </div>
     </>
   )
 }
