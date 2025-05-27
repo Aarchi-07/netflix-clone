563bc41fe7cbf2ffab873deea1ba8575f9ddf20f
Aarchi Kumari <aarchikumari07052@gmail.com>
2025-05-27 21:30:41 +0530

    fix movie api

--- app/api/movies/[movieId]/route.ts
+++ app/api/movies/[movieId]/route.ts
@@ -1,20 +1,18 @@
-import { NextResponse } from 'next/server';
+import { NextResponse, NextRequest } from 'next/server';
 import prismadb from "@/lib/prismadb";
 import serverAuth from "@/lib/serverAuth";
 
 export async function GET(
-    request: Request,
-    { params }: { params: { movieId: string } }
+    request: NextRequest,
+    context: { params: { movieId: string } }
 ) {
     try {
-        await serverAuth();
-
-        // Handle params validation
-        if (!params || !params.movieId) {
+        await serverAuth();        // Handle params validation
+        if (!context.params.movieId) {
             return new NextResponse('Invalid ID', { status: 400 });
         }
         
-        const { movieId } = params;
+        const movieId = context.params.movieId;
 
         const movie = await prismadb.movie.findUnique({
             where: {
